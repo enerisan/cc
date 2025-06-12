8c66222d80b0c36a0049a1006e169cabcb59e3e5
enerisan <enerisan@gmail.com>
2025-06-12 22:52:41 +0200

    feat:adding category icons to the incident card in webapp

--- webapp/src/main/java/enerisan/webapp/controller/WebAppController.java
+++ webapp/src/main/java/enerisan/webapp/controller/WebAppController.java
@@ -17,7 +17,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -40,11 +42,25 @@ public class WebAppController {
         if (user.getRole().getId() == 2) {
             model.addAttribute("user", user);
             List<IncidentWithCategoriesDto> incidentsWithCategories = incidentService.getAllIncidentsWithCategoriesByUserId(user.getId());
-
-
-
             model.addAttribute("incidents", incidentService.getAllIncidentsWithCategoriesByUserId((user.getId())));
+            Map<String, String> categoryIcons = new HashMap<>();
+            categoryIcons.put("Voirie", "fa-solid fa-road");
+            categoryIcons.put("Éclairage public", "fa-solid fa-lightbulb");
+            categoryIcons.put("Trottoirs", "fa-solid fa-shoe-prints");
+            categoryIcons.put("Conteneurs à déchets", "fa-solid fa-trash");
+            categoryIcons.put("Aire de jeux", "fa-solid fa-child");
+            categoryIcons.put("Fontaines", "fa-solid fa-faucet");
+            categoryIcons.put("Propreté", "fa-solid fa-broom");
+            categoryIcons.put("Gestion des déchets", "fa-solid fa-recycle");
+            categoryIcons.put("Espaces verts", "fa-solid fa-tree");
+            categoryIcons.put("Installations sportives", "fa-solid fa-dumbbell");
+            categoryIcons.put("Bâtiments publics", "fa-solid fa-building");
+            categoryIcons.put("Nuisibles", "fa-solid fa-bug");
+            categoryIcons.put("Égouts", "fa-solid fa-water");
+            categoryIcons.put("Circulation", "fa-solid fa-car-side");
+            categoryIcons.put("Autres", "fa-solid fa-ellipsis-h");
 
+            model.addAttribute("categoryIcons", categoryIcons);
             return new ModelAndView("user_dashboard");
         } else if (user.getRole().getId() == 1) {
             model.addAttribute("user", user);
