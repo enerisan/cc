31367b2d6cb3fe02b0ca2ca1ed6756ef29639fc0
enerisan <enerisan@gmail.com>
2025-07-03 15:48:28 +0200

    fix: fixing categories multiple select

--- webapp/src/main/resources/static/script.js
+++ webapp/src/main/resources/static/script.js
@@ -1,38 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const statusBtn = document.querySelector(".PageMain-centralBox-reports-filters-btn");
+    const filtersList = document.querySelector(".PageMain-centralBox-reports-filters-list");
+    const actives = document.querySelector(".PageMain-centralBox-reports-filters-active");
+    const checkboxes = document.querySelectorAll(".PageMain-centralBox-reports-filters-list input[type='checkbox']");
+    const cards = document.querySelectorAll(".ReportCard");
+    console.log("cards", cards);
+    console.log("filters", checkboxes);
 
-const statusBtn = document.querySelector(".PageMain-centralBox-reports-filters-btn");
-const filtersList = document.querySelector(".PageMain-centralBox-reports-filters-list");
-const actives = document.querySelector(".PageMain-centralBox-reports-filters-active")
-const checkboxes = document.querySelectorAll(".PageMain-centralBox-reports-filters-list input[type='checkbox']");
-const cards = document.querySelectorAll(".ReportCard");
-console.log("cards", cards);
-console.log("filters", checkboxes);
-//Show or hide filters
-const checkedFilters = [];
-//Gestion du filter faire apparaitre
-statusBtn.addEventListener("click", function () {
-    filtersList.classList.toggle("active");
+    const checkedFilters = [];
+
+    if (statusBtn && filtersList) {
+        statusBtn.addEventListener("click", function () {
+            filtersList.classList.toggle("active");
+        });
+    }
+
+    const showCards = (event) => {
+        const filterStatus = event.target.checked;
+        const filterValue = event.target.getAttribute("data-filter-status");
+        console.log(filterValue, filterStatus);
+        if (filterStatus) {
+            checkedFilters.push(filterValue);
+        } else {
+            const checkedFiltersIndex = checkedFilters.findIndex((filter) => filter === filterValue);
+            checkedFilters.splice(checkedFiltersIndex, 1);
+        }
+
+    };
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener("change", showCards);
+    });
+
+    const toggleButton = document.querySelector('.toggle-location');
+    if (toggleButton) {
+        toggleButton.addEventListener('click', () => {
+            toggleButton.classList.toggle('active');
+        });
+    }
+
+    const element = document.getElementById('categoryIds');
+
+    if (element) {
+        const choices = new Choices(element, {
+            removeItemButton: true,
+            searchEnabled: true,
+            itemSelectText: '',
+            shouldSort: false,
+            placeholder: true,
+            placeholderValue: 'Select categories',
+            classNames: {
+                containerOuter: 'choices',
+            }
+        });
+    }
 });
-const showCards = (event) => {
-    const filterStatus = event.target.checked;
-    const filterValue = event.target.getAttribute("data-filter-status");
-    console.log(event.target.getAttribute("data-filter-status"));
-    console.log(event.target.checked);
-    if (filterStatus) {
-        checkedFilters.push(filterValue)
-    }
-    if (!filterStatus) {
-        const checkedFiltersIndex = checkedFilters.findIndex((filter) => filter === filterValue);
-        checkedFilters.splice(checkedFiltersIndex, 1)
-        console.log("checkedFilters", checkedFilters)
-    }
-    /* const attribute = document */
-
-}
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener("change", showCards);
-
-
-})
-
-
-
